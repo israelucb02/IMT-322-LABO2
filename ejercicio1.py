391c6967372251a7902f98a351176c25f9044743
MaGabRam <136017727+MaGabRam@users.noreply.github.com>
2025-03-10 18:19:01 -0400

    Update ejercicio1.py

--- ejercicio1.py
+++ ejercicio1.py
@@ -39,22 +39,22 @@ def state_3():
     GPIO.output(led_pins[1], GPIO.HIGH)
 
 def state_4():
-    """State 4: This state turns off both LEDs"""
+    """State 4: This state turn
     GPIO.output(led_pins[0], GPIO.LOW)
     GPIO.output(led_pins[1], GPIO.LOW)
 
 def change_state():
     
     global state
-    state = (state % 4) + 1  # Cicla entre 1 y 4
+    state = (state % 4) + 1  # de 1 a 4
 
 def run():
     global state
     try:
         while True:
-            if GPIO.input(button_pin) == GPIO.LOW:  # Si se presiona el botón
+            if GPIO.input(button_pin) == GPIO.LOW:  # presionado el boton
                 change_state()
-                time.sleep(0.3)  # Esperar para evitar rebotes
+                time.sleep(0.3)  
             
             if state == 1:
                 state_1()
@@ -65,7 +65,7 @@ def run():
             elif state == 4:
                 state_4()
             
-            time.sleep(0.1)  # Pequeño retardo para optimizar el procesador
+            time.sleep(0.1)  # delay
     except KeyboardInterrupt:
         print("\nSaliendo del programa...")
         GPIO.cleanup()
